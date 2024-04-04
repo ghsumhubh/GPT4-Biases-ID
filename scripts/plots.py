560eec0632509c1fb974870beb0a95208c7ff356
River <tziony.i@gmail.com>
2024-04-04 18:26:38 +0300

    Update

--- scripts/plots.py
+++ scripts/plots.py
@@ -3,22 +3,22 @@ import pandas as pd
 from scripts.advantage import get_all_advantages_and_disadvantages
 import numpy as np
 
-def plot_single_reponse(df, only_use_col=None):
 
-    exclude_columns = ['Name', 'Score']  
+def plot_single_reponse(df, only_use_col=None):
+    exclude_columns = ['Name', 'Score']
 
     for column in df.columns:
         if column not in exclude_columns and (only_use_col is None or column == only_use_col):
             # Print unique values
             unique_values = df[column].unique()
-            
+
             # Print counts for each value, sorted from most to less
             value_counts = df[column].value_counts().sort_values(ascending=False)
-            
+
             # Calculate and print average score per category within the column, sorted from highest to lowest
             average_score_per_category = df.groupby(column)['Score'].mean().sort_values(ascending=False)
             summary_df = pd.DataFrame({
-                'Count': value_counts, 
+                'Count': value_counts,
                 'Average Score': average_score_per_category
             })
 
@@ -26,16 +26,23 @@ def plot_single_reponse(df, only_use_col=None):
             summary_df = summary_df.sort_values(by='Average Score', ascending=False)
 
             # Print the result
-            print(f"- - - {column} - - -\n{summary_df}\n")        
-            # Plotting average score per category, sorted
-            plt.figure(figsize=(10, 6))
-            average_score_per_category.sort_values().plot(kind='barh')  # Horizontal bar plot for better readability
+            print(f"- - - {column} - - -\n{summary_df}\n")
+
+            # Adjust figure size conditionally
+            if column == 'Ethnicity':
+                plt.figure(figsize=(10, 10))  # Taller figure for Ethnicity
+            else:
+                plt.figure(figsize=(10, 6))
+
+            # Plotting with a colormap for more colors
+            cmap = plt.get_cmap('viridis')  # Get a colormap
+            colors = cmap(np.linspace(0, 1, len(average_score_per_category)))  # Generate colors
+            average_score_per_category.sort_values().plot(kind='barh', color=colors)  # Use colors in plot
             plt.title(f'Average Score per {column}')
             plt.xlabel('Average Score')
             plt.ylabel(column)
             plt.tight_layout()
             plt.show()
-
 
 
 
